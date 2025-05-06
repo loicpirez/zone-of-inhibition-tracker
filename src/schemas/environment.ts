c0677307c8e49e65b20c603b571d232346661a16
Loïc PIREZ <loic@pirez.fr>
2025-05-06 17:28:35 +0900

    Change default port to 3001

--- src/schemas/environment.ts
+++ src/schemas/environment.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export const envSchema = z
 	.object({
-		PORT: z.string().regex(/^\d+$/, 'PORT must be a number').transform(Number).default('3000'),
+		PORT: z.string().regex(/^\d+$/, 'PORT must be a number').transform(Number).default('3001'),
 		UPLOAD_DIR: z.string().default(path.resolve(__dirname, '../../uploads')),
 		LOGS_DIR: z.string().default(path.resolve(__dirname, '../../logs')),
 		DATA_DIR: z.string().default(path.resolve(__dirname, '../../data')),
