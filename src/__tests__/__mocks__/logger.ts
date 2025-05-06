4ec49e18aaa595f5e62df59bf8756e16baf7df13
Loïc PIREZ <loic@pirez.fr>
2025-05-06 09:41:11 +0900

    Change log level for test logger output

--- src/__tests__/__mocks__/logger.ts
+++ src/__tests__/__mocks__/logger.ts
@@ -1,7 +1,7 @@
 const logger = {
-	error: jest.fn((...args) => console.error('[MOCK ERROR]', ...args)),
-	warn: jest.fn((...args) => console.warn('[MOCK WARN]', ...args)),
-	info: jest.fn((...args) => console.info('[MOCK INFO]', ...args)),
+	error: jest.fn((...args) => console.debug('[MOCK ERROR]', ...args)),
+	warn: jest.fn((...args) => console.debug('[MOCK WARN]', ...args)),
+	info: jest.fn((...args) => console.debug('[MOCK INFO]', ...args)),
 	debug: jest.fn((...args) => console.debug('[MOCK DEBUG]', ...args)),
 };
 
